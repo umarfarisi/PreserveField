9db7ef61533c1968bee381fa089a2125e4b0f27c
Muhammad Umar Farisi <pharese@gmail.com>
2019-04-21 22:47:36 +0700

    create FieldStorage, FieldStorageSharedPreference, and FieldStorageUtils

---
+++ savefield/src/main/java/com/umarfarisi/savefield/storage/FieldStorage.kt
@@ -0,0 +1,19 @@
+package com.umarfarisi.savefield.storage
+
+abstract class FieldStorage {
+
+    abstract fun get(storageName: String, key: String): Any?
+
+    abstract fun clear(storageName: String)
+
+    abstract fun put(storageName: String, key: String, value: Any)
+
+    abstract fun putAll(storageName: String, from: Map<String, Any>)
+
+    abstract fun remove(storageName: String, key: String)
+
+    abstract fun containsKey(storageName: String, key: String): Boolean
+
+    abstract fun getAll(storageName: String): Map<String, Any>
+
+}
